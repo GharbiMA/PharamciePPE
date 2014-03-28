853a2ae7f1f8c9fa70083cbeea03fe750f0911e4
Feker <Feker@PC-Feker.mshome.net>
2014-03-28 18:47:00 +0100

    Doctrine included safely annotation work in progress
    look at this : 
    http://wildlyinaccurate.com/integrating-doctrine-2-with-codeigniter-2

---
+++ application/models/Entity/CoordonneeGPS.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of CoordonneeGPS
+ *
+ * @author MedAmineGharbi
+ */
+class CoordonneeGPS extends CI_Model {
+    //put your code here
+    var $longitude;
+    var $lattitude;
+    function __construct() {
+        parent::__construct();
+    }
+}
---
+++ application/models/Entity/Garde.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Garde
+ *
+ * @author MedAmineGharbi
+ */
+class Garde extends CI_Model {
+    //put your code here
+    var $date;
+    function __construct() {
+        parent::__construct();
+    }
+}
